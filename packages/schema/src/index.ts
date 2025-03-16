8ee9d37ae061845ddaf9e887fb464b4bbb2a29fc
ElasticBottle <winstonyeo99@yahoo.com>
2025-03-16 13:24:50 +0100

    chore: update to react 19 chore: tailwind v4 feat: use pnpm for stability feat: tanstack router as base

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,13 +1,6 @@
-import { Schema } from "effect";
+import { type } from "arktype";
 
-export const OtpSchema = Schema.Struct({
-  code: Schema.String.pipe(
-    Schema.length(8, {
-      message: () => "Code must be 8 characters long.",
-    }),
-    Schema.pattern(/^[A-Z2-7]+$/, {
-      message: () => "Invalid Code provided, please check and try again.",
-    }),
-  ),
+export const OtpSchema = type({
+  code: "string > 8",
 });
-export type OtpSchema = typeof OtpSchema.Type;
+export type OtpSchema = typeof OtpSchema.infer;
