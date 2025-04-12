1df72a92bd1ece344c60de89f930bc842bafcb74
ElasticBottle <winstonyeo99@yahoo.com>
2025-04-12 18:45:57 +0200

    fix: type error

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,9 +1,9 @@
-import { dbContext } from "@/lib/hono";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { contextStorage } from "hono/context-storage";
 import { showRoutes } from "hono/dev";
 import { frontendRouter } from "../lib/frontend";
+import { dbContext } from "../lib/hono";
 import { apiRouter } from "./api";
 
 const app = new Hono()
