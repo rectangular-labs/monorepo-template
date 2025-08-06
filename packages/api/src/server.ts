91ba19144e4d0370ff96d12d05955796cb650f8a
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-06 15:15:10 -0400

    chore(api): add error loggers

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -2,27 +2,37 @@ import { experimental_ArkTypeToJsonSchemaConverter as ArkTypeToJsonSchemaConvert
 import { experimental_SmartCoercionPlugin as SmartCoercionPlugin } from "@orpc/json-schema";
 import { OpenAPIHandler } from "@orpc/openapi/fetch";
 import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
-import { createRouterClient } from "@orpc/server";
+import { createRouterClient, onError } from "@orpc/server";
 import { RPCHandler } from "@orpc/server/fetch";
 import {
   RequestHeadersPlugin,
   ResponseHeadersPlugin,
 } from "@orpc/server/plugins";
+import { createApiContext } from "./context";
 import { router } from "./routes";
 import { todoSchema } from "./routes/todo";
-import type { InitialContext } from "./types";
 
-export const serverClient = (context: InitialContext) =>
+export const serverClient = (context: Parameters<typeof createApiContext>[0]) =>
   createRouterClient(router, {
-    context,
+    context: () => createApiContext(context),
   });
 
 export const RpcHandler = new RPCHandler(router, {
   plugins: [new RequestHeadersPlugin(), new ResponseHeadersPlugin()],
+  interceptors: [
+    onError((error) => {
+      console.error("RPC Error:", error);
+    }),
+  ],
 });
 
 export const openAPIHandler = (apiUrl: string) =>
   new OpenAPIHandler(router, {
+    interceptors: [
+      onError((error) => {
+        console.error("OpenAPI Error:", error);
+      }),
+    ],
     plugins: [
       new RequestHeadersPlugin(),
       new ResponseHeadersPlugin(),
