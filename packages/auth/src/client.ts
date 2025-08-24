6bd2771fc6f1aba261ce5218516f8a6f432b0d38
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-24 22:09:11 +0200

    feat(auth): expose additional types from auth package for UI

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,13 +1,55 @@
-import { emailOTPClient } from "better-auth/client/plugins";
+import {
+  adminClient,
+  apiKeyClient,
+  emailOTPClient,
+  genericOAuthClient,
+  magicLinkClient,
+  multiSessionClient,
+  oidcClient,
+  oneTapClient,
+  organizationClient,
+  passkeyClient,
+  siweClient,
+  ssoClient,
+  twoFactorClient,
+  usernameClient,
+} from "better-auth/client/plugins";
 import { createAuthClient as createAuthClientBase } from "better-auth/react";
 import { authEnv } from "./env";
 
-export const createAuthClient = (): ReturnType<
+export type BaseAuthClient = ReturnType<typeof createAuthClientBase>;
+// used to generate the types for the complete auth client
+const _authClient = createAuthClientBase({
+  plugins: [
+    twoFactorClient(),
+    usernameClient(),
+    magicLinkClient(),
+    emailOTPClient(),
+    passkeyClient(),
+    genericOAuthClient(),
+    oneTapClient({
+      clientId: "",
+    }),
+    siweClient(),
+    // authorization
+    adminClient(),
+    apiKeyClient(),
+    // enterprise
+    organizationClient(),
+    oidcClient(),
+    ssoClient(),
+    // utility
+    multiSessionClient(),
+  ],
+});
+export type CompleteAuthClient = typeof _authClient;
+
+export type AuthClient = ReturnType<
   typeof createAuthClientBase<{
-    baseURL: string;
     plugins: [ReturnType<typeof emailOTPClient>];
   }>
-> => {
+>;
+export const createAuthClient = (): AuthClient => {
   const env = authEnv();
   return createAuthClientBase({
     baseURL: env.VITE_APP_URL,
