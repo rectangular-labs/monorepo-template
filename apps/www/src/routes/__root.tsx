593f3ed68018441b83063a8dc6e5e94b5fe03a57
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-30 14:54:29 +0200

    chore(ui): fix files exports

--- apps/www/src/routes/__root.tsx
+++ apps/www/src/routes/__root.tsx
@@ -10,7 +10,7 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { seo } from "~/lib/seo";
-import appCss from "../style.css?url";
+import appCss from "../styles.css?url";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
