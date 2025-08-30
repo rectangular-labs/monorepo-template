4501f0d1620ca82050a0fed4087cc1fe36ed4aec
ElasticBottle <winstonyeo99@yahoo.com>
2025-08-30 13:44:40 +0200

    fix(ui): shadcn cli working

--- packages/ui/src/components/auth/schema/code.ts
+++ packages/ui/src/components/auth/schema/code.ts
@@ -1,7 +1,7 @@
 import { type } from "arktype";
 
 export const CodeSchema = type("string").narrow((code, ctx) => {
-  if (code.length !== 6 || Number.isNaN(parseInt(code))) {
+  if (code.length !== 6 || Number.isNaN(parseInt(code, 10))) {
     return ctx.reject({ expected: "a valid code", actual: "" });
   }
   return true;
