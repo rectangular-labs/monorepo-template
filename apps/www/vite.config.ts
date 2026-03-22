4f4ad67982956ea1ae7fdf9f17d6f317ba2afbc9
ElasticBottle <winstonyeo99@yahoo.com>
2026-03-22 17:41:43 +0100

    feat: add env validation in pre build step

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,13 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
 import { defineConfig } from "vite-plus";
-
-// const jiti = createJiti(import.meta.url);
-// const env = await jiti.import("./src/lib/env");
-// // parses all the required env vars (server env is a super set of client env)
-// (env as { serverEnv: () => ReturnType<typeof serverEnv> }).serverEnv();
 
 const config = defineConfig({
   resolve: {
@@ -15,7 +9,6 @@ const config = defineConfig({
   },
   plugins: [
     tailwindcss(),
-    mkcert(),
     tanstackStart({
       customViteReactPlugin: true,
     }),
@@ -24,10 +17,6 @@ const config = defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-  },
-  server: {
-    proxy: {},
-    port: 6969,
   },
 });
 
